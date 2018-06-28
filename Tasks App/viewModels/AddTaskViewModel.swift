3e8fc6bd5ca5f7fc51a3a1f3c73b3cb3dcfe8142
Mina Yousry <mina.yousry.samir@gmail.com>
2018-06-29 02:56:59 +0300

    get categories from core data

--- Tasks App/viewModels/AddTaskViewModel.swift
+++ Tasks App/viewModels/AddTaskViewModel.swift
@@ -7,8 +7,29 @@
 //
 
 import UIKit
+import CoreData
 
 class AddTaskViewModel: NSObject {
 
+    var categories = [TaskCategory]()
     
+    func categoriesNumber() -> Int{
+        return categories.count
+    }
+    
+    func categoryNameForRow(row: Int) -> String {
+        return categories[row].name!
+    }
+    
+    func fetchCategories(completion:()->()) {
+        let fetchCategoriesRequest: NSFetchRequest<TaskCategory> = TaskCategory.fetchRequest()
+        do{
+            let fetshedCategories = try PersistenceUtility.context.fetch(fetchCategoriesRequest)
+            self.categories = fetshedCategories
+            completion()
+        }catch{
+            
+        }
+        
+    }
 }
--- Tasks App/views/AddTaskViewController.swift
+++ Tasks App/views/AddTaskViewController.swift
@@ -5,14 +5,33 @@
 //  Created by Admin on 6/28/18.
 //  Copyright © 2018 dp. All rights reserved.
 //
+//Users/admin/Documents/IOS Projects/Tasks App/Tasks App.xcodeproj
 
 import UIKit
 
-class AddTaskViewController: UIViewController {
-
+class AddTaskViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+    
+    @IBOutlet var addTaskViewModel: AddTaskViewModel!
+    @IBOutlet var categoriesPickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Add Task"
+        addTaskViewModel.fetchCategories {
+            self.categoriesPickerView.reloadAllComponents()
+        }
     }
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return addTaskViewModel.categoriesNumber()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        return addTaskViewModel.categoryNameForRow(row: row)
+    }
+    
 }
