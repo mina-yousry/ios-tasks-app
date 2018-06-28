727491bcbb717e97b3f1bde576bda76ef04bfe68
Mina Yousry <mina.yousry.samir@gmail.com>
2018-06-28 22:29:18 +0300

    add a task design

---
+++ Tasks App/viewModels/AddTaskViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  AddTaskViewModel.swift
+//  Tasks App
+//
+//  Created by Admin on 6/28/18.
+//  Copyright © 2018 dp. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewModel: NSObject {
+
+    
+}
